28c5caf5a56d714caa3508ed246e9ee280f10931
Amandine <amandine.dismoimedia@gmail.com>
2022-03-02 11:58:15 +0100

    modif

--- variables.py
+++ variables.py
@@ -41,3 +41,97 @@ print(multiline_text)
 
 exemple = "programme terminé"
 print(exemple)
+
+nombre = -123
+print(nombre)
+print(type(nombre))
+
+nombre = -123.0
+print(nombre)
+print(type(nombre))
+
+nombre = "5.71"
+print(type(nombre))
+print(type(nombre))
+nombre = float(nombre)
+print(nombre)
+(type(nombre))
+
+nombre1 = "5"
+print(nombre1)
+print(type(nombre1))
+nombre = int(nombre)
+print(nombre1)
+print(type(nombre1))
+
+nombre2 = 3.14
+print(nombre2)
+print(type(nombre2))
+nombre2 = int(nombre2)
+print(nombre2)
+print(type(nombre2))
+
+texte = str(nombre)
+print(texte)
+print(type(texte))
+
+my_var = 0
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 1
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 123
+my_var = bool(my_var)
+print(my_var)
+
+my_var = -1
+my_var = bool(my_var)
+print(my_var)
+
+m_var = -123
+my_var= bool(my_var)
+print(my_var)
+
+my_var = 0.0
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 0.001
+my_var = bool(my_var)
+print(my_var)
+
+my_var = " "
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "0"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [123, "abc", False]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [False]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = []
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [None]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = None
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [[]]
+my_var = bool(my_var)
+print(my_var)
+
